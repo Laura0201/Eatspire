3cd856bde6e55caaf45541b755d6b9501d9360ed
Laura0201 <laura.mangliers03@gmail.com>
2025-04-22 14:40:18 +0200

    Commit 4

--- app/src/main/java/BusinessLogik/Pizza.java
+++ app/src/main/java/BusinessLogik/Pizza.java
@@ -1,8 +1,11 @@
 package BusinessLogik;
 
+import java.util.ArrayList;
+
 public class Pizza extends Gericht{
     private double preis;
     private String name;
+    private ArrayList<Zutat> zutatenListe = new ArrayList<>();
     Pizza(String Name, double Preis){
         name=Name;
         preis=Preis;
