061572464b9bb6a0dd29ccc0867004f8b1457e14
jona1404 <wi24283@lehre.dhbw-stuttgart.de>
2025-04-22 14:50:40 +0200

    Jona ist auch da

--- app/src/main/java/BusinessLogik/Main.java
+++ app/src/main/java/BusinessLogik/Main.java
@@ -3,6 +3,6 @@ package BusinessLogik;
 public class Main {
     public static void main(String[]  args){
         User Jona = new User("jt.buettner", "JTZ67(@erkjlsg");
-        System.out.println(Jona.passwortKorrekt("jt.buettner", "ksdögkdj"));
+
     }
 }
