177b4623e292c6077b25593d0ad9417c238a71a5
jona1404 <wi24283@lehre.dhbw-stuttgart.de>
2025-05-06 15:16:57 +0200

    06.06. erster Push während der Arbeit

--- app/src/main/java/com/example/eatspire/MainActivity1.java
+++ app/src/main/java/com/example/eatspire/MainActivity1.java
@@ -24,12 +24,9 @@ import java.text.BreakIterator;
 
 
 
-/// /Das ist alles nur random aus der vorlesung!!!
+
 public class MainActivity1 extends ComponentActivity {
-    private EditText _editTextCustomerName;
-    private EditText _editTextDeviceName;
-    private CheckBox _checkBoxAgb;
-    private Button _buttonNew;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +34,9 @@ public class MainActivity1 extends ComponentActivity {
         enableEdgeToEdge();
         setContentView(R.layout.activity_main);
 
-        _editTextCustomerName = findViewById(R.id.edit_text_customer_name);
-        _editTextDeviceName = findViewById(R.id.edit_text_device_name);
-        _checkBoxAgb = findViewById(R.id.check_box_agb_accept);
-        _buttonNew = findViewById(R.id.button_new_order);
 
-        _buttonNew.setOnClickListener(view ->{
-            String customerName = _editTextCustomerName.getText().toString();
-            String deviceName = _editTextDeviceName.getText().toString();
+        };
 
-            boolean agbAccepted = _checkBoxAgb.isChecked();
-            if(!agbAccepted){
-
-            }
-
-            // Handle the button click event here
-            // For example, you can start a new activity or show a message
-            // based on the input values.
-        });
-    }
 
     private void enableEdgeToEdge() {
         // Implement your edge-to-edge functionality here
