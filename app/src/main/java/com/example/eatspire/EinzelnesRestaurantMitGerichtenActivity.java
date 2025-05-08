44c82b79ed58952a2c76a5629a1c2a82b18cc7e8
jona1404 <wi24283@lehre.dhbw-stuttgart.de>
2025-05-08 16:08:33 +0200

    08.05. Vorarbeit zur Loogik der Aktivities

--- app/src/main/java/com/example/eatspire/EinzelnesRestaurantMitGerichtenActivity.java
+++ app/src/main/java/com/example/eatspire/EinzelnesRestaurantMitGerichtenActivity.java
@@ -1,8 +1,10 @@
 package com.example.eatspire;
 
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,17 +23,71 @@ public class EinzelnesRestaurantMitGerichtenActivity extends ComponentActivity {
     private int idDesRestaurants;
     private String pfadFürBildDesRestaurants=getPfadfürBildDesRestaurants(idDesRestaurants);
 private ImageView bildDesRestaurants;
-private TextView nameDesRestaurants, beschreibungDesRestaurants;
+private TextView nameDesRestaurants;
+private TextView beschreibungDesRestaurants;
 private Button buttonVorspeisen, buttonNachspeisen, buttonHauptspeisen, buttonHeißgetränke, buttonKaltgetränke;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.einzelnes_restaurant_mit_gerichten_view);
 
+        nameDesRestaurants = findViewById(R.id.textRestaurantName);
+        beschreibungDesRestaurants = findViewById(R.id.textRestaurantDescription);
+        bildDesRestaurants = findViewById(R.id.imageRestaurant);
+        buttonVorspeisen=findViewById(R.id.buttonVorspeisen);
+        buttonNachspeisen=findViewById(R.id.buttonDesserts);
+        buttonHauptspeisen=findViewById(R.id.buttonHauptspeisen);
+        buttonHeißgetränke=findViewById(R.id.buttonHeissgetraenke);
+        buttonKaltgetränke=findViewById(R.id.buttonKaltgetraenke);
+
+        nameDesRestaurants.setText(getNameDesRestaurants(idDesRestaurants));
         Bitmap bitmap = BitmapFactory.decodeFile(pfadFürBildDesRestaurants);
         bildDesRestaurants.setImageBitmap(bitmap);
+        beschreibungDesRestaurants.setText(getBeschrebibungDesRestaurants(idDesRestaurants));
+
+//        buttonVorspeisen.setOnClickListener(v -> {
+//            Intent intent = new Intent(EinzelnesRestaurantMitGerichtenActivity.this, GerichteAnzeigenTypEgal.class);
+//            startActivity(intent); Parameter übergeben, id und Art der Gerichte
+//        });
+        if(MainActivity1.getController().model.getDatamanager().getRestaurant(idDesRestaurants).getVorspeisenListe().isEmpty()){
+            buttonVorspeisen.setVisibility(View.GONE);//Unterschied zu View.Invisible
+        }
+//        buttonHauptspeisen.setOnClickListener(v -> {;
+//            Intent intent = new Intent(EinzelnesRestaurantMitGerichtenActivity.this, GerichteAnzeigenTypEgal.class);
+//            startActivity(intent); // Parameter übergeben, id und Art der Gerichte
+//        });
+        if(MainActivity1.getController().model.getDatamanager().getRestaurant(idDesRestaurants).getHauptspeisenListe().isEmpty()){
+            buttonHauptspeisen.setVisibility(View.GONE);//Unterschied zu View.Invisible
+        }
+//        buttonNachspeisen.setOnClickListener(v -> {;
+//            Intent intent = new Intent(EinzelnesRestaurantMitGerichtenActivity.this, GerichteAnzeigenTypEgal.class);
+//            startActivity(intent); // Parameter übergeben, id und Art der Gerichte
+//        });
+        if(MainActivity1.getController().model.getDatamanager().getRestaurant(idDesRestaurants).getNachspeisenListe().isEmpty()){
+            buttonNachspeisen.setVisibility(View.GONE);//Unterschied zu View.Invisible
+        }
+//        buttonHeißgetränke.setOnClickListener(v -> {;
+//            Intent intent = new Intent(EinzelnesRestaurantMitGerichtenActivity.this, GerichteAnzeigenTypEgal.class);
+//            startActivity(intent); // Parameter übergeben, id und Art der Gerichte
+//        });
+        if(MainActivity1.getController().model.getDatamanager().getRestaurant(idDesRestaurants).getHeißgetränkeListe().isEmpty()){
+            buttonHeißgetränke.setVisibility(View.GONE);//Unterschied zu View.Invisible
+        }
+//        buttonKaltgetränke.setOnClickListener(v -> {;
+//            Intent intent = new Intent(EinzelnesRestaurantMitGerichtenActivity.this, GerichteAnzeigenTypEgal.class);
+//            startActivity(intent); // Parameter übergeben, id und Art der Gerichte
+//        });
+        if(MainActivity1.getController().model.getDatamanager().getRestaurant(idDesRestaurants).getKaltgetränkeListe().isEmpty()){
+            buttonKaltgetränke.setVisibility(View.GONE);//Unterschied zu View.Invisible
+        }
 
     }
     private String getPfadfürBildDesRestaurants(int indexAlsId){
         return MainActivity1.getController().model.getDatamanager().getPfadFürImageDesRestaurants(indexAlsId);
+    }
+    private String getNameDesRestaurants(int indexAlsId){
+        return MainActivity1.getController().model.getDatamanager().getRestaurant(indexAlsId).getName();
+    }
+    private String getBeschrebibungDesRestaurants(int indexAlsId){
+        return MainActivity1.getController().model.getDatamanager().getRestaurant(indexAlsId).getBeschreibung();
     }
 }
